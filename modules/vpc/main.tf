5b7e087858ef0c07a6d5cb0addf383c063a66a6f
johnmcphillips <john.mcphillips@gmail.com>
2025-03-11 21:44:47 -0500

    vpc

---
+++ modules/vpc/main.tf
@@ -0,0 +1,68 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "PublicSubnet01" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PublicSubnet01.cidr_block
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = var.PublicSubnet01.name
+  }
+}
+
+resource "aws_subnet" "PublicSubnet02" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PublicSubnet02.cidr_block
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = var.PublicSubnet02.name
+  }
+}
+
+resource "aws_subnet" "PublicSubnet03" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PublicSubnet03.cidr_block
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = var.PublicSubnet03.name
+  }
+}
+
+resource "aws_subnet" "PrivateSubnet01" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PrivateSubnet01.cidr_block
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = var.PrivateSubnet01.name
+  }
+}
+
+resource "aws_subnet" "PrivateSubnet02" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PrivateSubnet02.cidr_block
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = var.PrivateSubnet02.name
+  }
+}
+
+resource "aws_subnet" "PrivateSubnet03" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.PrivateSubnet03.cidr_block
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = var.PrivateSubnet03.name
+  }
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,61 @@
+variable "vpc_name" {
+  default     = "main"
+}
+
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "PublicSubnet01" {
+  description = "PublicSubnet01"
+  type        = map(string)
+  default = {
+    "name"       = "PublicSubnet01"
+    "cidr_block" = "10.0.101.0/24"
+    }
+}
+
+variable "PublicSubnet02" {
+  description = "PublicSubnet02"
+  type        = map(string)
+  default = {
+    "name"       = "PublicSubnet02"
+    "cidr_block" = "10.0.102.0/24"
+    }
+}
+
+variable "PublicSubnet03" {
+    description = "PublicSubnet03"
+    type = map(string)
+    default = {
+        "name" = "PublicSubnet03"
+        "cidr_block" = "10.0.103.0/24"
+    }
+}
+
+variable "PrivateSubnet01" {
+  description = "PrivateSubnet01"
+  type        = map(string)
+  default = {
+    "name"       = "PrivateSubnet01"
+    "cidr_block" = "10.0.1.0/24"
+  }
+}
+
+variable "PrivateSubnet02" {
+  description = "PrivateSubnet02"
+  type        = map(string)
+  default = {
+    "name"       = "PrivateSubnet02"
+    "cidr_block" = "10.0.2.0/24"
+  }
+}
+
+variable "PrivateSubnet03" {
+  description = "PrivateSubnet03"
+  type        = map(string)
+  default = {
+    "name"       = "PrivateSubnet03"
+    "cidr_block" = "10.0.3.0/24"
+  }
+}
